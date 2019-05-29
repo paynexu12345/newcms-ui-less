9ea49dbd76a46822dc6bf2d304999613ce0ebd65
Payne Xu <payne.xu@neulion.cn>
2019-05-29 18:40:46 +0800

    2

--- projects/ng-poppins/src/lib/components/comp-utils.ts
+++ projects/ng-poppins/src/lib/components/comp-utils.ts
@@ -19,6 +19,7 @@ export function commonInitCfg(component, cfg) {
 export function applyMixins(derivedCtor: any, baseCtors: any[]) {
   baseCtors.forEach(baseCtor => {
       Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
+          if(!derivedCtor.prototype[name])
           derivedCtor.prototype[name] = baseCtor.prototype[name];
       });
   });
